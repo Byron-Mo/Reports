1bab46e50ce8fcd20dbe5d601620930074fedfc9
Byron Mo <byronmo@gmail.com>
2016-05-21 01:50:36 -0700

    set up seed date

--- app/assets/javascripts/transactions.js
+++ app/assets/javascripts/transactions.js
@@ -1,8 +1,44 @@
-var categories = ["Taxes", "Restaurants", "Business", "Transportation", "Accommodations"];
-var vendors = [""];
-var years = [2016, 2015];
-var days = [];
+var categories = ["Taxes", "Restaurants", "Business", "Transportation", "Accommodations"],
+    vendors = ["Walmart", "Target", "Best Buy", "Home Depot", "Macy's", "Radio Shack"],
+    years = [2016, 2015],
+    days = [],
+    months = ["jan", "feb", "mar", "apr", "may", "jun", "jul", "aug", "sep", "oct", "nov", "dec"],
+    transactions = [];
+
 for (var i = 1; i <= 31; i++) {
   days.push(i)
 }
-var months = ["jan", "feb", "mar", "apr", "may", "jun", "jul", "aug", "sep", "oct", "nov", "dec"];
+
+var randomItem = function(arr) {
+  var randNum = Math.floor(Math.random() * arr.length);
+  return arr[randNum];
+}
+
+var selectCost = function() {
+  var num = parseFloat((Math.random() * 100).toFixed(2));
+  num *= Math.floor(Math.random() * 2) === 0 ? 1 : -1
+  return num
+}
+
+var randomDate = function() {
+  var iso;
+  while (iso === undefined || isNaN(iso)) {
+    iso = Date.parse(randomItem(months) + " " + randomItem(days) + " " + randomItem(years))
+  }
+  return iso
+}
+
+for (var i = 1; i <= 700; i++) {
+  var transaction = {
+    id: i,
+    date: randomDate(),
+    cost: selectCost(),
+    category: randomItem(categories),
+    vendor: randomItem(vendors)
+  };
+
+  transactions.push(transaction)
+}
+
+// console.log(transactions[0].date)
+module.exports = transactions;
--- frontend/reports.jsx
+++ frontend/reports.jsx
@@ -1,5 +1,6 @@
 var React = require('react'),
-    ReactDOM = require('react-dom');
+    ReactDOM = require('react-dom'),
+    transactions = require('../app/assets/javascripts/transactions');
 
 var Reports = React.createClass({
   render: function() {
